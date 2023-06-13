9e5ebb705a376eae14bc498f3cdbd500d45a2c0e
RexPerG <rezaggholamali@gmail.com>
2023-06-13 14:22:23 +0100

    Firebase config completed

---
+++ firebase.config.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAU0NM8mvK6ncUhbP6VUck5PzXrEw9AzQM",
+  authDomain: "git-connected-2229a.firebaseapp.com",
+  projectId: "git-connected-2229a",
+  storageBucket: "git-connected-2229a.appspot.com",
+  messagingSenderId: "536737870416",
+  appId: "1:536737870416:web:c93c54a6cc167e575a1755"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export const db = getFirestore()
