770e740904d4aa3cb92e64d7f635693516dc8143
alicjafleszar <alicjafleszar@icloud.com>
2023-06-15 14:49:50 +0100

    update signin page

--- Pages/SignIn.js
+++ Pages/SignIn.js
@@ -1,4 +1,4 @@
-import { signInWithPopup, getAdditionalUserInfo } from "firebase/auth";
+import { signInWithPopup, getAdditionalUserInfo, onAuthStateChanged } from "firebase/auth";
 import { auth, provider } from "../firebase.config";
 import { Button } from 'react-native';
 import { useContext } from "react";
@@ -6,21 +6,29 @@ import { UserContext } from "../contexts/User";
 
 export default function SignIn() {
   const { setUser } = useContext(UserContext)
+  const { setIsSignedIn } = useContext(UserContext)
+
+  onAuthStateChanged(auth, signedInUser => {
+    if (signedInUser) {
+      setIsSignedIn(true)
+    } else {
+      setIsSignedIn(false)
+    }
+  })
 
   const handleLogIn = async () => {
     const result = await signInWithPopup(auth, provider)
     const userInfo = getAdditionalUserInfo(result)
     setUser(userInfo)
-    console.log(userInfo)
-    console.log(userInfo.isNewUser)
   }
 
+
   return (
-    <Button
-      title="Login"
-      onPress={
-        handleLogIn
-      }
-    />
+    <>
+      <Button
+        title="Log in"
+        onPress={ handleLogIn }
+      />
+    </>
   );
 }
