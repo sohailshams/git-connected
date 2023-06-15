1c509caecea8362a34607ad6d5e8e5564eaa7635
Mason <mason@Masons-MacBook-Air-2.local>
2023-06-15 15:11:10 +0100

    function added

--- utils/functions.js
+++ utils/functions.js
@@ -1,1 +1,33 @@
- 
+import db from '../firebase.config' 
+
+export const addUser = async (
+   username,
+   avatar_url,
+   html_url,
+   name,
+   location,
+   bio,
+   email,
+   id
+ ) => {
+   const newName = !name ? "" : name;
+   const newLocation = !location ? "" : location;
+   const newBio = !bio ? "" : bio;
+   const newEmail = !email ? "" : email;
+
+   try {
+     const docRef = await setDoc(doc(collection(db, "users"), `${username}`), {
+       username: username,
+       avatar_url: avatar_url,
+       html_url: html_url,
+       name: newName,
+       location: newLocation,
+       bio: newBio,
+       email: newEmail,
+       id,
+     });
+     console.log("document written", docRef.id);
+   } catch (e) {
+     console.log(e);
+   }
+ };
