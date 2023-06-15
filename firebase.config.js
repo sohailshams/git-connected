49b2470c4d4abb386b107bba926e96eed1a677e9
Mason <mason@Masons-MacBook-Air-2.local>
2023-06-15 15:07:22 +0100

    start

--- firebase.config.js
+++ firebase.config.js
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+import { getFirestore, collection, getDocs } from 'firebase/firestore';
 import { getAuth, GithubAuthProvider } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -22,6 +22,6 @@ const auth = getAuth(app)
 
 const provider = new GithubAuthProvider(app)
 
-const db = getFirestore()
+const db = getFirestore(app)
 
 export { auth, db, provider }
