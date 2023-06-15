c0697cfc256d5b60a84225d422225e4d77ffd00a
alicjafleszar <alicjafleszar@icloud.com>
2023-06-15 14:48:13 +0100

    update user context - add isSignedIn state

--- contexts/User.js
+++ contexts/User.js
@@ -3,11 +3,11 @@ import { createContext, useState } from "react";
 export const UserContext = createContext()
 
 export const UserProvider = ({ children }) => {
-    const [user, setUser] = useState(null)
-
+    const [user, setUser] = useState({})
+    const [isSignedIn, setIsSignedIn] = useState(false)
 
     return (
-        <UserContext.Provider value={{ user, setUser }}>
+        <UserContext.Provider value={{ user, setUser, isSignedIn, setIsSignedIn }}>
             {children}
         </UserContext.Provider>
 )}
