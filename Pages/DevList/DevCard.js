73e4dac70e3032a65ea9de49eacc83093aba0919
sohailshams <sohailshamas@yahoo.com>
2023-06-19 16:54:59 +0100

    Implement getting list of langages & rendering in devCard component.

--- Pages/DevList/DevCard.js
+++ Pages/DevList/DevCard.js
@@ -1,9 +1,20 @@
-import { View, Text, Image, Linking, ScrollView } from "react-native";
-import React from "react";
+import { View, Text, Image, Linking, ScrollView, FlatList } from "react-native";
+import React, { useEffect, useState } from "react";
+import { getDevLanguages, makeUniqueArray } from "../../utils/functions";
 
 const DevCard = ({ data }) => {
+  const [lang, setLang] = useState([]);
+
+  useEffect(() => {
+    const getLangArray = async () => {
+      const fetchedLangArray = await getDevLanguages(data.item.username);
+      setLang(fetchedLangArray);
+    };
+    getLangArray();
+  }, []);
+
   return (
-    <View className="flex flex-row border-[1px] border-black m-2 p-5 items-center">
+    <View className="flex flex-row border-[1px] border-black m-2 p-5 items-center mx-auto w-[315px]">
       <Image
         className="h-[80px] w-[80px] rounded-full"
         source={data.item.avatar_url}
@@ -18,6 +29,16 @@ const DevCard = ({ data }) => {
         >
           GitHub
         </Text>
+      </View>
+      <View className="flex flex-row flex-wrap w-[150px]">
+        <FlatList
+          data={makeUniqueArray(lang[data.item.username])}
+          renderItem={({ item }) => (
+            <Text className="py-1 px-2 ml-3 border-[1px] border-black my-1">
+              {item}
+            </Text>
+          )}
+        />
       </View>
     </View>
   );
