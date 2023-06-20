f29fd6366a6924e668f67db7358ff6cf00b963cb
RexPerG <rezaggholamali@gmail.com>
2023-06-20 16:57:33 +0100

    Added languages to profile page (beta)

--- Pages/Profile/Profile.js
+++ Pages/Profile/Profile.js
@@ -1,17 +1,19 @@
-import { useContext, useEffect, useState } from "react";
-import { UserContext } from "../../contexts/User";
-import { Text, View, Button, Image } from "react-native";
-import { signOut } from "firebase/auth";
-import { auth } from "../../firebase.config";
-import FormButtons from "../../components/ProfilePage/FormButtons";
-import ProfileData from "../../components/ProfilePage/ProfileData";
+import { useContext, useEffect, useState } from 'react';
+import { UserContext } from '../../contexts/User';
+import { Text, View, Button, Image, FlatList } from 'react-native';
+import { signOut } from 'firebase/auth';
+import { auth } from '../../firebase.config';
+import FormButtons from '../../components/ProfilePage/FormButtons';
+import ProfileData from '../../components/ProfilePage/ProfileData';
 import {
+  getDevLanguages,
   getPortfolioById,
   getProjectById,
   getUserById,
-} from "../../utils/functions";
-import MiniRepoList from "../../components/MiniLists/MiniRepoList";
-import MiniColabList from "../../components/MiniLists/MiniColabList";
+  makeUniqueArray,
+} from '../../utils/functions';
+import MiniRepoList from '../../components/MiniLists/MiniRepoList';
+import MiniColabList from '../../components/MiniLists/MiniColabList';
 
 const Profile = ({ navigation }) => {
   const handleSignOut = () => {
@@ -20,7 +22,7 @@ const Profile = ({ navigation }) => {
   const [count, setCount] = useState(0);
   const { user } = useContext(UserContext);
   const [portfolio, setPortfolio] = useState([]);
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
   const [colab, setColab] = useState([]);
   const [state, setState] = useState(1);
 
@@ -33,12 +35,41 @@ const Profile = ({ navigation }) => {
   useEffect(() => {
     getProjectById(user.id).then((data) => setColab(data));
   }, [count]);
+
+  const [lang, setLang] = useState([]);
+  console.log(lang);
+  useEffect(() => {
+    const getLangArray = async () => {
+      const fetchedLangArray = await getDevLanguages(user.username);
+      console.log('user:', user, 'fetchedLangArray:', fetchedLangArray)
+      setLang(fetchedLangArray);
+    };
+    getLangArray();
+  }, []);
+
   return (
     <View className="w-5/6 bg-white mx-auto rounded-md shadow-lg mt-3">
-      <ProfileData user={data} />
+      <View className="flex flex-row pl-3">
+        <Image
+          className="rounded-full"
+          source={user.avatar_url}
+          style={{ height: 50, width: 50 }}
+        />
+        {/* <ProfileData user={data} /> */}
+        <View className="h-20 w-10 flex flex-wrap space-evenly">
+          <FlatList
+            data={makeUniqueArray(lang[data.username])}
+            renderItem={({ item }) => (
+              <Text className="py-1 px-2 ml-3 border-[1px] border-black my-1">
+                {item}
+              </Text>
+            )}
+          />
+        </View>
+      </View>
       <Button
         title="add repo"
-        onPress={() => navigation.navigate("add Repo", { count, setCount })}
+        onPress={() => navigation.navigate('add Repo', { count, setCount })}
       />
       <FormButtons
         title1="Showcase list"
@@ -54,7 +85,7 @@ const Profile = ({ navigation }) => {
       <Button title="Sign out" onPress={handleSignOut} />
       <Button
         title="edit"
-        onPress={() => navigation.navigate("Edit", { data, count, setCount })}
+        onPress={() => navigation.navigate('Edit', { data, count, setCount })}
       />
     </View>
   );
--- components/ProfilePage/ProfileData.js
+++ components/ProfilePage/ProfileData.js
@@ -1,12 +1,39 @@
-import { View, Image, Text } from "react-native"
+import { View, Image, Text } from 'react-native';
+
 const ProfileData = ({ user }) => {
-    return(
-    <View>
-      <Text className='text-red-500'>{user.username}</Text>
-      <Image source={user.avatar_url} style={{ height: 50, width: 50 }} />
+  console.log('in ProfileData', user)
+  return (
+    <View className="mt-3">
+      <View className="flex flex-row pl-3">
+        <Image
+          className="rounded-full"
+          source={user.avatar_url}
+          style={{ height: 50, width: 50 }}
+        />
+        {/* <View className="grow"> */}
+          <View className='flex flex-wrap'>
+            <Text className='w-[200px]'>JavaScript</Text>
+            <Text className='w-[200px]'>Python</Text>
+            <Text className='w-[200px]'>HTML</Text>
+            <Text className='w-[200px]'>CSS</Text>
+            <Text className='w-[200px]'>Python</Text>
+            <Text className='w-[200px]'>HTML</Text>
+            <Text className='w-[200px]'>CSS</Text>
+            <Text className='w-[200px]'>JavaScript</Text>
+            <Text className='w-[200px]'>Python</Text>
+            <Text className='w-[200px]'>HTML</Text>
+            <Text className='w-[200px]'>CSS</Text>
+            <Text className='w-[200px]'>Python</Text>
+            <Text className='w-[200px]'>HTML</Text>
+            <Text className='w-[200px]'>CSS</Text>
+          </View>
+        {/* </View> */}
+      </View>
+
+      <Text className="text-red-500">{user.username}</Text>
       <Text>{user.bio}</Text>
       <Text>{user.location}</Text>
     </View>
-    )
-}
-export default ProfileData
+  );
+};
+export default ProfileData;
