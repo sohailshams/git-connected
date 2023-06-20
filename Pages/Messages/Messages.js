32f392f86968ee336967a042475ca6304298fd02
alicjafleszar <alicjafleszar@icloud.com>
2023-06-20 22:13:23 +0100

    add messages page component

---
+++ Pages/Messages/Messages.js
@@ -0,0 +1,12 @@
+import { Text } from "react-native"
+import { View } from "react-native-web"
+
+const Messages = ({navigation}) => {
+    return (
+        <View>
+            <Text>Messages</Text>
+        </View>
+    )
+}
+
+export default Messages
