4dd9d4449d7fc237dd194259f5c1227c65c6fdf3
RexPerG <rezaggholamali@gmail.com>
2023-06-20 14:57:24 +0100

    Added components to tailwind.config.js; also changed username text to red as a test in ProfileData.js

--- components/ProfilePage/ProfileData.js
+++ components/ProfilePage/ProfileData.js
@@ -2,7 +2,7 @@ import { View, Image, Text } from "react-native"
 const ProfileData = ({ user }) => {
     return(
     <View>
-      <Text>{user.username}</Text>
+      <Text className='text-red-500'>{user.username}</Text>
       <Image source={user.avatar_url} style={{ height: 50, width: 50 }} />
       <Text>{user.bio}</Text>
       <Text>{user.location}</Text>
