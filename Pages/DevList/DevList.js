0a4d8703e8db15df779b6b1a198b407be67433b9
sohailshams <sohailshamas@yahoo.com>
2023-06-21 15:38:41 +0100

    Added styles for dev list and updated navigation title.

--- Pages/DevList/DevList.js
+++ Pages/DevList/DevList.js
@@ -3,7 +3,7 @@ import React, { useEffect, useState } from "react";
 import { getDevList } from "../../utils/functions";
 import DevCard from "./DevCard";
 
-const DevList = ({navigation}) => {
+const DevList = ({ navigation }) => {
   const [devArray, setDevArray] = useState([]);
   useEffect(() => {
     const getDevArray = async () => {
@@ -17,9 +17,18 @@ const DevList = ({navigation}) => {
     return <DevCard data={item} navigation={navigation} />;
   };
   return (
-    <ScrollView>
-      <View>
-        <FlatList data={devArray} renderItem={renderItem} />
+    <ScrollView className="w-5/6 max-[768px]:w-[95%] bg-white mx-auto rounded-md shadow-lg my-3">
+      <View className="mt-3 w-full">
+        <FlatList
+          contentContainerStyle={{
+            flexDirection: "row",
+            flexWrap: "wrap",
+            justifyContent: "center",
+            gap: "10px",
+          }}
+          data={devArray}
+          renderItem={renderItem}
+        />
       </View>
     </ScrollView>
   );
