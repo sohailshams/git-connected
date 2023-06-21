937076d95d370312cf0c1e4798c2b7337c2819f7
sohailshams <sohailshamas@yahoo.com>
2023-06-21 08:59:13 +0100

    Added conditional logic to change bg-color of buttons.

--- components/ProfilePage/FormButtons.js
+++ components/ProfilePage/FormButtons.js
@@ -1,6 +1,7 @@
 import { View, Text, Button, TouchableOpacity } from "react-native";
 
 const FormButtons = ({ title1, title2, state, setState }) => {
+  console.log("stat>>>>>>", state);
   const handleButton1 = () => {
     setState(1);
   };
@@ -11,14 +12,20 @@ const FormButtons = ({ title1, title2, state, setState }) => {
     <View className="flex flex-row my-3 max-[375px]:flex-col">
       <TouchableOpacity
         className={
-          "bg-zinc-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
+          state === 1
+            ? "bg-lime-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
+            : "bg-zinc-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
         }
         onPress={handleButton1}
       >
         <Text className="text-white">{title1}</Text>
       </TouchableOpacity>
       <TouchableOpacity
-        className="bg-zinc-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
+        className={
+          state === 2
+            ? "bg-lime-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
+            : "bg-zinc-500 shadow-2xl py-1 px-2 w-[150px] rounded-full text-center my-1 ml-3"
+        }
         onPress={handleButton2}
       >
         <Text className="text-white">{title2}</Text>
